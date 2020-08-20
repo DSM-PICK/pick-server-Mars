bcb3d7e5cf291d85426bf910b7b38724b03b80ce
Jungwoo-Son <jungwoo9356@gmail.com>
2020-08-20 15:24:28 +0900

    [ADD] App initialization

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express();
-
+const init_app = require('./loaders');
 const configs = require('./configs');
 
-
+const app = express();
+init_app(app);
 
 app.listen(configs.PORT, ()=> {
     console.log(`server starting at ${configs.PORT}...`)
