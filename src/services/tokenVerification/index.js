6d4b68844264aca9cf3836a9fa8cb40007e29cb3
Jungwoo-Son <jungwoo9356@gmail.com>
2020-08-20 21:27:42 +0900

    [UPDATE] refactorying code, funciton isExpired and isSelfContained

--- src/services/tokenVerification/index.js
+++ src/services/tokenVerification/index.js
@@ -4,30 +4,47 @@ const { json } = require('express');
 
 const { TOKEN_SECRET } = require('../../configs');
 
-module.exports = (token) => {
-    const splited_token = token.split('.');
-  
-    const header = splited_token[0];
-    const payload = splited_token[1];
-    const signature = splited_token[2];
 
+function isExpired(payload) {
+    const token_exp_time = getExpTimeByPayload(payload);
+    const today_time =  new Date().getTime() / 1000;
+    if(token_exp_time < today_time ) {
+        return true;
+    }
+    return false;
+}
+function getExpTimeByPayload(payload) {
+    return JSON.parse(Buffer.from(payload,'base64').toString()).exp;
+}
 
-    const parsed_payload = JSON.parse(Buffer.from(payload,'base64').toString());
-    if(parsed_payload.exp < new Date().getTime() / 1000) {
+function isSelfContained(token_data) {
+    const valid_signature = signToken(token_data.header, token_data.payload, TOKEN_SECRET);
+    if(token_data.signature != valid_signature) {
         return false;
     }
-
-    const secrete = TOKEN_SECRET;
-    const binary_secrete = Buffer.from(secrete, 'base64');
-
-    const valid_signature = crypto.createHmac('sha256', binary_secrete)
+    return true;
+}
+function signToken(header, payload, secret) {
+    const binary_secrete = Buffer.from(secret, 'base64');
+    return crypto.createHmac('sha256', binary_secrete)
         .update(header + '.' + payload)
         .digest('base64')
         .replace(/=/gi, '')
         .replace(/\+/gi, '-')
         .replace(/\//gi, '_');
+}
 
-    if(signature != valid_signature) {
+module.exports = (token) => {
+    const splited_token = token.split('.');  
+    const header = splited_token[0];
+    const payload = splited_token[1];
+    const signature = splited_token[2];
+
+    if(isExpired(payload)) {
+        return false;
+    }
+
+    if(isSelfContained({header, payload, signature}) == false) {
         return false;
     }
 
