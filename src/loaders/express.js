ed6ae9b6b86c6421519e820870f6626c71144a2e
Jungwoo-Son <jungwoo9356@gmail.com>
2020-08-23 01:10:10 +0900

    [ADD] middlewares

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-
+const middlewares = require('../apis/middlewares');
 
 module.exports = (app) => {
     app.use(cors());
     app.use(express.json());
+
+    app.use(middlewares);
     
-    
+    app.get('/', (req, res) => {
+        res.send('hello');
+    });
 };
