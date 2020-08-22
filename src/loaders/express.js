c07b2785619c217ac00296c46d4139f570d51968
Jungwoo-Son <jungwoo9356@gmail.com>
2020-08-23 01:20:59 +0900

    [ADD] Error handler

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const middlewares = require('../apis/middlewares');
+const logger = require('./logger');
 
 module.exports = (app) => {
     app.use(cors());
@@ -11,4 +12,18 @@ module.exports = (app) => {
     app.get('/', (req, res) => {
         res.send('hello');
     });
+
+
+    app.use((err, req, res, next) => {
+        const status = err.status || 500;
+        logger.error(
+            `${req.method} ${req.url} ${status} : ${err.message}`
+        );
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            status: err.status
+        });
+    });
+
 };
