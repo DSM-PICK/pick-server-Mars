b5a286eec6dbce0361022f4e912fea99583a9246
Jungwoo-Son <jungwoo9356@gmail.com>
2020-08-23 01:35:18 +0900

    [FIX] uncorrect way to recieve authentication data

--- src/apis/middlewares/tokenVerification.js
+++ src/apis/middlewares/tokenVerification.js
@@ -3,7 +3,7 @@ const verifyToken = require('../../services/tokenVerification');
 
 module.exports = (req, res, next) => {
     try{
-        verifyToken(req.body.Authentication);
+        verifyToken(req.get('Authentication'));
         next();
     } catch(e) {
         next(e);
