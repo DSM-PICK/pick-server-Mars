e51898a95027fbb7537c2991e510f8abf362cdf6
Jungwoo-Son <jungwoo9356@gmail.com>
2020-08-24 22:32:23 +0900

    [ADD] DB_DATABASE, DB_USERNAME

--- src/configs/index.js
+++ src/configs/index.js
@@ -4,6 +4,8 @@ if (process.env.NODE_ENV != 'not_dev') {
 
 module.exports = {
     PORT: process.env.PORT,
+    DB_DATABASE: process.env.DB_DATABASE,
+    DB_USERNAME: process.env.DB_USERNAME,
     DB_PASSWORD: process.env.DB_PASSWORD,
     TOKEN_SECRET: process.env.TOKEN_SECRET
 }
