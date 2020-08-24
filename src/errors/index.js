c95528b34f7aed1a4a516cc6f741646b369a2e83
Jungwoo-Son <jungwoo9356@gmail.com>
2020-08-25 01:47:27 +0900

    [ADD] InvalidDate, NotFoundDataInThisDate for Activity Service

--- src/errors/index.js
+++ src/errors/index.js
@@ -10,10 +10,15 @@ class HttpError extends Error {
 const ExpiredToken = new HttpError('expired access token', 410, '');
 const InvaildToken = new HttpError('invaild access token', 401, '');
 
+const InvalidDate = new HttpError('invalid date. Shape of date is yyyy-mm-dd.', 400, '');
+const NotFoundDataInThisDate = new HttpError('Not found data in given date', 404, '');
 
 module.exports = {
     ExpiredToken,
-    InvaildToken
+    InvaildToken,
+
+    InvalidDate,
+    NotFoundDataInThisDate
 }
 
 
