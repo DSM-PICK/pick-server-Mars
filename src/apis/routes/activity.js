9738cdb4627b0a839c5567631fe90571e678f799
Jungwoo-Son <jungwoo9356@gmail.com>
2020-08-25 01:48:08 +0900

    [ADD] complete /dates/:date api

--- src/apis/routes/activity.js
+++ src/apis/routes/activity.js
@@ -1,8 +1,30 @@
 const { Router } = require('express');
-
 const route = Router();
 
-route.get('/dates/:date', (req, res) => {
-    
+const ActivityRepository  = require('../../repositories').Activity;
+const Teacher = require('../../repositories').Teacher;
+const { ActivityService, Exceptions } = require('../../services/activityService');
+
+const { InvalidDate, NotFoundDataInThisDate } = require('../../errors');
+
+route.get('/dates/:date', async (req, res, next) => {
+    const service = new ActivityService(ActivityRepository, Teacher);  
+
+    try {
+        const result = await service.getThisDateActivity(req.params.date);    
+        res.send(result);
+    } catch (e) {
+        if(e instanceof Exceptions.InvalidDateException) {
+            next(InvalidDate);
+        }
+        else if(e instanceof Exceptions.NotFoundDataException) {
+            next(NotFoundDataInThisDate);
+        }
+        else {
+            next(e);
+        }
+    }
 
 });
+
+module.exports = route;
