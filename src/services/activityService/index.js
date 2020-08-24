e7a80270601a630f8699d6c95635ecd5c65fbe5b
Jungwoo-Son <jungwoo9356@gmail.com>
2020-08-24 20:44:52 +0900

    [ADD] feature that check invalid date

--- src/services/activityService/index.js
+++ src/services/activityService/index.js
@@ -1,11 +1,44 @@
 class ActivityService {
 
+    async getThisDateActivity(str_date) {
+        if(isValidDate(str_date) == false) {
+            throw new InvalidDateException;
+        }
+
+
+
+    }
+
 }
 
+function isValidDate(str) {
+	if  ( !/([0-9]{4})-([0-9]{2})-([0-9]{2})/.test(str) )  {
+		return false;
+	}
+	
+	var splited_date = str.split('-');
+	var max_day = new Date(new Date(splited_date[0], splited_date[1], 1) - 86400000).getDate();
+	
+	if  ( splited_date[1] < 1 || splited_date[1] > 12  )  {
+		return false;
+	}
+	if  ( splited_date[2] > max_day )  {
+		return false;
+	}
+	return true;
+}
 
-const InvalidDateException = new Error('Given date is not valid');
-const NotFoundDataException = new Error('Couldn\'t found data in given date');
+class InvalidDateException extends Error {
+    constructor() {
+        super('Given date is not valid');
+    }
+}
 
+class NotFoundDataException extends Error{
+    constructor() {
+        super('Couldn\'t found data in given date');
+    }
+}
 module.exports = {
     ActivityService,
     Exceptions: {
