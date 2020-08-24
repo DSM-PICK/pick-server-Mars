6194a18746ebb1ac6e8b7ca743a570020794317e
Jungwoo-Son <jungwoo9356@gmail.com>
2020-08-25 00:35:10 +0900

    [UPDATE] change some spec

--- src/tests/fakes/fakeActivityRepository.js
+++ src/tests/fakes/fakeActivityRepository.js
@@ -1,7 +1,20 @@
 const { Activity } = require('../../repositories');
 
 class FakeActivityRepository extends Activity {
-
+    static async findOne(option) {
+        if(option.where.date.getTime() == new Date('2020-08-24').getTime()) {
+            return {
+                dataValues: {
+                  date: '2020-08-24',
+                  schedule: 'club',
+                  second_floor_teacher_id: 'Kim',
+                  third_floor_teacher_id: 'Ahn',
+                  forth_floor_teacher_id: 'Jwa'
+                }
+              };
+        }
+        return null;
+    }
 }
 
 module.exports = FakeActivityRepository;
