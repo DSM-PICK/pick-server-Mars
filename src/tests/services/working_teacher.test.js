897905d319e351ea2b5da0a18841b7768c84400a
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-07 19:42:44 +0900

    [ADD] new case, about invalid floor

--- src/tests/services/working_teacher.test.js
+++ src/tests/services/working_teacher.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const WorkingTeacherService = require('../../services/activityService');
+const WorkingTeacherService = require('../../services/WorkingTeacherService');
 const Exceptions = require('../../errors/servicesExceptions');
 const AcvitiyRepository = require('../fakes/fakeActivityRepository');
 const TeacherRepository = require('../fakes/fakeTeacherRepository');
@@ -11,17 +11,17 @@ describe('Working Teacher Service Test', () => {
     describe('Get Working Teacher by date and floor', () => {
         describe('valid result', () => {
             const correct_f2_result = {
-                "date": 2020-08-24,
+                "date": "2020-08-24",
                 "working_teacher": "김정은",
                 "floor": 2
             };
             const correct_f3_result = {
-                "date": 2020-08-24,
+                "date": "2020-08-24",
                 "working_teacher": "안소희",
                 "floor": 3
             };
             const correct_f4_result = {
-                "date": 2020-08-24,
+                "date": "2020-08-24",
                 "working_teacher": "좌찬익",
                 "floor": 4
             };
@@ -59,7 +59,17 @@ describe('Working Teacher Service Test', () => {
             it('Couldn\'t found data in the date', async () => {
                 await assert.rejects(working_teacher_service.getWorkingTeacherByDateAndFloor(new Date('2020-08-25'),2),
                             Exceptions.NotFoundDataException);
-            })
+            });
+
+            it('floor is too small', async () => {
+                await assert.rejects(working_teacher_service.getWorkingTeacherByDateAndFloor(new Date('2020-08-24'),1),
+                            Exceptions.InvalidFloorException);
+            });
+
+            it('floor is too big', async () => {
+                await assert.rejects(working_teacher_service.getWorkingTeacherByDateAndFloor(new Date('2020-08-24'),5),
+                            Exceptions.InvalidFloorException);
+            });
         });
         
     });
