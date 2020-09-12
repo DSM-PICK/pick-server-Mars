400646ec761a3703669af058958709681eaf0e06
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-12 14:15:30 +0900

    [ADD] Exchage working teacher test

--- src/tests/services/working_teacher.test.js
+++ src/tests/services/working_teacher.test.js
@@ -74,4 +74,78 @@ describe('Working Teacher Service Test', () => {
         
     });
 
+    describe('Exchange working teacher\'s date', () => {
+        const exist_input1 = {
+            date: new Date('2020-08-24'),
+            floor: 2,
+            teacher_name: '김정은'
+        };
+        const exist_input2 = {
+            date: new Date('2020-09-12'),
+            floor: 3,
+            teacher_name: '이진혁'
+        };
+
+        const correct_result_date1 = {
+            date: '2020-08-24',
+            schedule: 'club',
+            second_floor_teacher_id: 'Lee',
+            third_floor_teacher_id: 'Ahn',
+            forth_floor_teacher_id: 'Jwa'
+        };
+        const correct_result_date2 = {
+            date: '2020-09-12',
+            schedule: 'club',
+            second_floor_teacher_id: 'Son',
+            third_floor_teacher_id: 'Kim',
+            forth_floor_teacher_id: 'Yoo'
+        };
+
+        const ivalid_date_input = {
+            date: new Date('2020-01-22'),
+            floor: 3,
+            teacher_name: '김정은'
+        };
+        const ivalid_floor_input = {
+            date: new Date('2020-08-24'),
+            floor: 5,
+            teacher_name: '김정은'
+        };
+        const mismatch_teacher_input = {
+            date: new Date('2020-08-24'),
+            floor: 5,
+            teacher_name: '김정은'
+        };
+
+        it('success exchange', async () => {
+            try {
+                const updated_activities = await working_teacher_service.exchangeTeacher(exist_input1, exist_input2);
+
+                assert.deepEqual(updated_activities[0], correct_result_date1);
+                assert.deepEqual(updated_activities[1], correct_result_date2);
+            }
+            catch(e) {
+                assert.fail(e.message);
+            }
+        });
+
+        describe('invalid result', () => {
+            it('not found activity', async () => {
+                await assert.rejects(working_teacher_service.exchangeTeacher(ivalid_date_input, ivalid_date_input),
+                            Exceptions.NotFoundDataException);
+            });
+
+            it('given invalid floor', async () => {
+                await assert.rejects(working_teacher_service.exchangeTeacher(ivalid_floor_input, ivalid_floor_input),
+                            Exceptions.InvalidFloorException);
+            });
+
+            it('mismatch teacher\'s name', async () => {
+                await assert.rejects(working_teacher_service(mismatch_teacher_input, mismatch_teacher_input,
+                            Exceptions.MismatchToRelationDatas));
+            });
+        });
+
+
+    });
 });
