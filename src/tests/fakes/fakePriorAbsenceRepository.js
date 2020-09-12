c40d9f22b91f54c41fdf7f3db30dca23cb20d3b1
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-12 22:46:43 +0900

    [ADD] some test case

---
+++ src/tests/fakes/fakePriorAbsenceRepository.js
@@ -0,0 +1,26 @@
+const Exceptions = require('../../errors/repositoriesExceptions');
+
+
+
+class FakePriorAbsenceRepository {
+    static async findPriorAbsenceByDate(date) {
+        if(date.getTime() == new Date('2020-08-24').getTime()) {
+            return [
+                {
+                    id: 1,
+                    teacher_id: 'Kim',
+                    start_date: new Date('2020-08-24'),
+                    end_date: new Date('2020-08-24'),
+                    student_num: 2411,
+                    start_period: 7,
+                    end_period: 10,
+                }
+            ];
+        }
+        throw new Exceptions.NotFoundDataException;
+    }
+}
+
+
+
+module.exports = FakePriorAbsenceRepository;
--- src/tests/services/prior_absence.test.js
+++ src/tests/services/prior_absence.test.js
@@ -5,20 +5,30 @@ const Exceptions = require('../../errors/servicesExceptions');
 const PriorAbsenceRepository = require('../fakes/fakePriorAbsenceRepository');
 
 describe('prior absence test', () => {
+    const prior_absence_service = new PriorAbsenceService(PriorAbsenceRepository);
     describe('get prior absence by date test', () => {
         const correct_result = [
                 {
                     stdnum: 2411,
-                    start_date: 2020/08/13,
+                    start_date: new Date("2020-08-24"),
                     start_period: 7,
-                    end_date: 2020/08/16,
+                    end_date: new Date("2020-08-24"),
                     end_period: 10
                 }
             ];
         it('valid result', async () => {
             try {
-                const result = await PriorAbsenceService.getPriorAbsenceByDate(new Date('2020-08-24'));
+                const result = await prior_absence_service.getPriorAbsenceByDate(new Date('2020-08-24'));
                 assert.deepEqual(result, correct_result);
+            }
+            catch(e) {
+                assert.fail(e.message);
+            }
+        });
+        it('valid result, no datas', async () => {
+            try {
+                const result = await prior_absence_service.getPriorAbsenceByDate(new Date('2020-08-25'));
+                assert.deepEqual(result, []);
             }
             catch(e) {
                 assert.fail(e.message);
