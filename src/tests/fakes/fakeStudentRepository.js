7e2ae9cf9b3648582fac3b392422c43a2b622d77
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-16 19:28:27 +0900

    [ADD] test student data

--- src/tests/fakes/fakeStudentRepository.js
+++ src/tests/fakes/fakeStudentRepository.js
@@ -1,36 +1,42 @@
-
+const students = [{
+    num: 1101,
+    name: '김대웅',
+    club_name: '팬텀',
+    class_name: '1-1',
+},
+{
+    num: 1111,
+    name: '손정우',
+    club_name: '모딥',
+    class_name: '1-1',
+},
+{
+    num: 2401,
+    name: '김대웅',
+    club_name: 'DMS',
+    class_name: '2-4',
+},
+{
+    num: 2411,
+    name: '손정우',
+    club_name: 'UP',
+    class_name: '2-4',
+},
+{
+    num: 2417,
+    name: '이진혁',
+    club_name: 'UP',
+    class_name: '2-4',
+}];
 
 
 class FakeStudentRepository {
     constructor() {
-        this.students = [{
-            num: 1111,
-            name: '손정우',
-            club_name: '모딥',
-            class_name: '1-1',
-        },
-        {
-            num: 2411,
-            name: '손정우',
-            club_name: 'UP',
-            class_name: '2-4',
-        },
-        {
-            num: 2401,
-            name: '김대웅',
-            club_name: 'DMS',
-            class_name: '2-4',
-        },
-        {
-            num: 2417,
-            name: '이진혁',
-            club_name: 'UP',
-            class_name: '2-4',
-        }];
+        
     }
 
-    async findAllByNum(num) {
-        const finded_students = this.students.filter( (student) => {
+    static async findAllByNum(num) {
+        const finded_students = students.filter( (student) => {
             if(String(student.num).indexOf(num) != -1) {
                 return true;
             }
@@ -39,8 +45,8 @@ class FakeStudentRepository {
         return finded_students;
     }
 
-    async findAllByName(name) {
-        const finded_students = this.students.map( (student) => {
+    static async findAllByName(name) {
+        const finded_students = students.filter( (student) => {
             if(student.name.indexOf(name) != -1) {
                 return true;
             }
@@ -49,12 +55,13 @@ class FakeStudentRepository {
         return finded_students;
     }
 
-    async findAllByNumberAndName(num, name) {
-        const finded_students = this.students.map( (student) => {
+    static async findAllByNumberAndName(num, name) {
+        const finded_students = students.filter( (student) => {
             if(student.name.indexOf(name) != -1 && String(student.num).indexOf(num) != -1) {
                 return true;
             }
         });
+        return finded_students;
     }
 }
 
