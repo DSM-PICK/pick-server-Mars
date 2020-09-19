87855fe044e952aca0be634bedd21379505f81c1
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-19 17:36:20 +0900

    [ADD] None Token

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,19 +1,19 @@
 class HttpError extends Error {
-    constructor(message, status, code) {
-      super(message);
-      this.status = status;
-      this.code = code;
+    constructor(message, status) {
+        super(message);
+        this.status = status;
     }
 }
 
+const NoneToken = new HttpError('none access token', 400);
+const ExpiredToken = new HttpError('expired access token', 410);
+const InvaildToken = new HttpError('invaild access token', 401);
 
-const ExpiredToken = new HttpError('expired access token', 410, '');
-const InvaildToken = new HttpError('invaild access token', 401, '');
-
-const InvalidDate = new HttpError('invalid date. Shape of date is yyyy-mm-dd.', 400, '');
-const NotFoundDataInThisDate = new HttpError('Not found data in given date', 404, '');
+const InvalidDate = new HttpError('invalid date. Shape of date is yyyy-mm-dd.', 400);
+const NotFoundDataInThisDate = new HttpError('Not found data in given date', 404);
 
 module.exports = {
+    NoneToken,
     ExpiredToken,
     InvaildToken,
 
@@ -22,4 +22,4 @@ module.exports = {
 }
 
 
-  
+
