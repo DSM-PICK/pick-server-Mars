705ce605d885406e5e3ec050cccb3e3ba828d8bf
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-19 19:27:08 +0900

    [ADD] Get /months/:month

--- src/apis/routes/activity.js
+++ src/apis/routes/activity.js
@@ -3,15 +3,17 @@ const route = Router();
 
 const ActivityRepository  = require('../../repositories').Activity;
 const Teacher = require('../../repositories').Teacher;
-const { ActivityService, Exceptions } = require('../../services/activityService');
+const ActivityService = require('../../services/activityService');
+const Exceptions = require('../../errors/servicesExceptions');
 
 const { InvalidDate, NotFoundDataInThisDate } = require('../../errors');
 
-route.get('/dates/:date', async (req, res, next) => {
-    const service = new ActivityService(ActivityRepository, Teacher);  
 
+const service = new ActivityService(ActivityRepository, Teacher);  
+
+route.get('/dates/:date', async (req, res, next) => {
     try {
-        const result = await service.getThisDateActivity(req.params.date);    
+        const result = await service.getThisDateActivity(new Date(req.params.date));    
         res.send(result);
     } catch (e) {
         if(e instanceof Exceptions.InvalidDateException) {
@@ -26,5 +28,27 @@ route.get('/dates/:date', async (req, res, next) => {
     }
 
 });
+
+route.get('/months/:month', async (req, res, next) => {
+    try {
+        const result = await service.getThisMonthActivity(Number.parseInt(req.params.month));
+        res.send(result);
+    }
+    catch(e) {
+        console.log(e);
+        if(e instanceof Exceptions.InvalidDateException) {
+            next(InvalidDate);
+        }
+        else if(e instanceof Exceptions.NotFoundDataException) {
+            next(NotFoundDataInThisDate);
+        }
+        else {
+            next(e);
+        }
+    }
+});
+
+
+
 
 module.exports = route;
