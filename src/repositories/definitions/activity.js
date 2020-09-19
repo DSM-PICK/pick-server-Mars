6e185529554aba5a25d1a7ccc8e3a6c2b380b57b
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-19 19:26:44 +0900

    [FIX] import sequelzie.op

--- src/repositories/definitions/activity.js
+++ src/repositories/definitions/activity.js
@@ -1,4 +1,4 @@
-const { Sequelize, DataTypes, Model } = require('sequelize');
+const { Sequelize, DataTypes, Model, Op } = require('sequelize');
 const { sequelize } = require('../../loaders/database');
 const Errors = require('../../errors/repositoriesExceptions');
 
