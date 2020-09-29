a32036c77c0547c332ea1c4b70a279e8a4cb8257
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-30 00:53:04 +0900

    [UPDATE] remove verbose message

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,21 +1,21 @@
 class HttpError extends Error {
-    constructor(message, status) {
-        super(message);
+    constructor(status) {
+        // super(message);
         this.status = status;
     }
 }
 
-const NoneToken = new HttpError('none access token', 400);
-const ExpiredToken = new HttpError('expired access token', 410);
-const InvaildToken = new HttpError('invaild access token', 401);
+const NoneToken = new HttpError(401);
+const ExpiredToken = new HttpError(410);
+const InvaildToken = new HttpError(403);
 
-const InvalidDate = new HttpError('invalid date. Shape of date is yyyy-mm-dd.', 400);
-const NotFoundDataInThisDate = new HttpError('Not found data in given date', 404);
+const InvalidDate = new HttpError(400);
+const NotFoundDataInThisDate = new HttpError(404);
 
 
-const InvalidFloor = new HttpError('invalid floor. Range of floor must be 2~4.', 400);
-const NotFoundDataInThisFloor = new HttpError('Not found data in given floor', 404);
-const NotFoundTeacher = new HttpError('Not found Teacher given date', 400);
+const InvalidFloor = new HttpError(400);
+const NotFoundDataInThisFloor = new HttpError(404);
+const NotFoundTeacher = new HttpError(404);
 
 module.exports = {
     NoneToken,
