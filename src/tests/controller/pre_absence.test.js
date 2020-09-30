182a4b33a0f67d64e80955aca64de9a0c6e53b51
Jungwoo-Son <jungwoo9356@gmail.com>
2020-10-01 01:41:26 +0900

    [ADD] getPreAbsenceByDate test

---
+++ src/tests/controller/pre_absence.test.js
@@ -0,0 +1,78 @@
+const assert = require('assert');
+
+const Request = require('../fakes/fakeRequest');
+const Response = require('../fakes/fakeResponse');
+const Repository = require('../fakes/fakePriorAbsenceRepository');
+const { getPreAbsenceByDate, createPreAbsence } = require('../../apis/routes/pre_absence')(Repository);
+
+const Exceptins = require('../../errors');
+
+const exceptionThrower = (e) => {
+    throw e;
+};
+
+
+describe('pre_absence api test', () => {
+    describe('getPreAbsenceByDate', () => {
+        describe('success', () => {
+            it('find data', async () => {
+
+                const req = new Request({ body: { date: '2020-08-24' } });
+                const expected_res = new Response({
+                    result:
+                        [
+                            {
+                                id: 1,
+                                student_num: 2411,
+                                start_date: '2020-08-24',
+                                start_period: 7,
+                                end_date: '2020-08-24',
+                                end_period: 10,
+                            }
+                        ]
+                });
+                const res = new Response();
+                try {
+                    await getPreAbsenceByDate(req, res, exceptionThrower);
+                    assert.deepStrictEqual(expected_res, res);
+                } catch (e) {
+                    assert.fail(e);
+                }
+            });
+        });
+        describe('fail', () => {
+            it('not found', async () => {
+                const req = new Request({ body: { date: '2020-08-25' } });
+                
+                try {
+                    await getPreAbsenceByDate(req, new Response, exceptionThrower);   
+                    assert.fail('it shouldn\'t have do well');
+                } catch (e) {
+                    if(e == Exceptins.NotFound) {
+                        assert.ok();
+                    }
+                    else {
+                        assert.fail(e);
+                    }
+                }
+            });
+            it('bad request', async () => {
+                const req = new Request({ body: { date: '2020-8-25' } });
+                
+                try {
+                    await getPreAbsenceByDate(req, new Response, exceptionThrower);   
+                    assert.fail('it shouldn\'t have do well');
+                } catch (e) {
+                    if(e == Exceptins.BadRequest) {
+                        assert.ok();
+                    }
+                    else {
+                        assert.fail(e);
+                    }
+                }
+            });
+        });
+    });
+});
+
+
