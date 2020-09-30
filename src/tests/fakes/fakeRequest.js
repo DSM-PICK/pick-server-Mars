fe2016db125271e6f23482d45709dcff82e6429e
Jungwoo-Son <jungwoo9356@gmail.com>
2020-10-01 00:46:34 +0900

    [ADD] fakeResquest, fakeResponse for testing controller

---
+++ src/tests/fakes/fakeRequest.js
@@ -0,0 +1,14 @@
+class FakeRequest {
+    constructor(data) {
+        this.body = data.body;
+        this.params = data.params;
+    }
+
+    static compare(req1, req2) {
+        return JSON.stringify(req1.params) == JSON.stringify(req2.params)
+            && JSON.stringify(req1.body) == JSON.stringify(req2.body);
+    }
+
+}
+
+module.exports = FakeRequest;
---
+++ src/tests/fakes/fakeResponse.js
@@ -0,0 +1,12 @@
+
+class FakeResponse {
+    constructor() {
+        
+    }
+
+    send(result) {
+        this.result = result;
+    }
+}
+
+module.exports = FakeResponse;
