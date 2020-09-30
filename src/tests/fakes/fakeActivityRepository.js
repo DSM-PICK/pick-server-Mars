f32865107710b8daa076c9a72d989cc703876349
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-30 21:41:22 +0900

    [FIX] remove useless statement, comment

--- src/tests/fakes/fakeActivityRepository.js
+++ src/tests/fakes/fakeActivityRepository.js
@@ -59,45 +59,6 @@ class FakeActivityRepository {
             throw new Exceptions.NotFoundDataException;
         }
         return results;
-
-        // const result_last_year = [
-        //     {
-        //         date: "2019-12-24",
-        //         schedule: "club",
-        //         second_floor_teacher_id: 'Kim',
-        //         third_floor_teacher_id: 'Ahn',
-        //         forth_floor_teacher_id: 'Ahn'
-        //     },
-        // ];
-        // const result_this_year = [
-        //     {
-        //         date: "2020-08-24",
-        //         schedule: "club",
-        //         second_floor_teacher_id: 'Kim',
-        //         third_floor_teacher_id: 'Ahn',
-        //         forth_floor_teacher_id: 'Ahn'
-        //     },
-        // ];
-        // const result_next_year = [
-        //     {
-        //         date: "2021-01-24",
-        //         schedule: "club",
-        //         second_floor_teacher_id: 'Kim',
-        //         third_floor_teacher_id: 'Ahn',
-        //         forth_floor_teacher_id: 'Ahn'
-        //     },
-        // ];
-        // if(year == 2019){
-        //    return result_last_year;
-        // }
-        // if(year == 2020) {
-        //     return result_this_year;
-        // }
-        // if(year == 2021) {
-        //     return result_next_year;
-        // }
-
-        throw new Exceptions.NotFoundDataException;
     }
 
     static async updateAutoDetect(activity) {
