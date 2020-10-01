4d7b1e9f2682245f4022d6bb5fb0917fb65d8a6a
Jungwoo-Son <jungwoo9356@gmail.com>
2020-10-01 22:38:20 +0900

    [ADD] route mapping

--- src/apis/routes/index.js
+++ src/apis/routes/index.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
 const activity = require('./activity');
 const working_teacher =  require('./workingTeacher');
+const pre_aabsence = require('./pre_absence')(require('../../repositories').PriorAbsence);
+const student = require('./student')(require('../../repositories').Student);
+
 
 const router = Router();
 
@@ -10,6 +13,12 @@ router.get('/activity/dates/:date', activity.getActivityByDate);
 
 router.get('/working-teacher/today/:floor', working_teacher.getTodayWorkingTeacher);
 router.patch('/working-teacher/exchange', working_teacher.exchangeTeacher);
+
+router.get('/pre-absence/date/:date', pre_aabsence.getPreAbsenceByDate);
+router.post('/pre-absence', pre_aabsence.createPreAbsence);
+
+router.get('/student/autocomplete/:incomplete', student.getAutoCompleteStudent);
+
 
 
 module.exports = router;
