309baa9c9d7557cd3e9f9cd5ca40693e4c3dd0e6
Jungwoo-Son <jungwoo9356@gmail.com>
2020-10-02 02:11:00 +0900

    [ADD] route deletePreAbsence mapping

--- src/apis/routes/index.js
+++ src/apis/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const activity = require('./activity');
 const working_teacher =  require('./workingTeacher');
-const pre_aabsence = require('./pre_absence')(require('../../repositories').PriorAbsence);
+const pre_absence = require('./pre_absence')(require('../../repositories').PriorAbsence);
 const student = require('./student')(require('../../repositories').Student);
 
 
@@ -14,8 +14,9 @@ router.get('/activity/dates/:date', activity.getActivityByDate);
 router.get('/working-teacher/today/:floor', working_teacher.getTodayWorkingTeacher);
 router.patch('/working-teacher/exchange', working_teacher.exchangeTeacher);
 
-router.get('/pre-absence/date/:date', pre_aabsence.getPreAbsenceByDate);
-router.post('/pre-absence', pre_aabsence.createPreAbsence);
+router.get('/pre-absence/date/:date', pre_absence.getPreAbsenceByDate);
+router.post('/pre-absence', pre_absence.createPreAbsence);
+router.delete('/pre-absence/:id', pre_absence.deletePreAbsence);
 
 router.get('/student/autocomplete/:incomplete', student.getAutoCompleteStudent);
 
