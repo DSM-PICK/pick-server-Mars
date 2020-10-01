150900b17c803ccec27bc02784989041134c5394
Jungwoo-Son <jungwoo9356@gmail.com>
2020-10-02 00:10:12 +0900

    [ADD] authExtractor

---
+++ src/apis/middlewares/authExtractor.js
@@ -0,0 +1,21 @@
+function separateToken(token) {
+    const splited_token = token.split('.');  
+
+    return {
+        header: splited_token[0],
+        payload: splited_token[1],
+        signature: splited_token[2]
+    }
+}
+
+function getIdByPayload(payload) {
+    return JSON.parse(Buffer.from(payload,'base64').toString()).id;
+}
+
+
+
+module.exports = (req, res, next) => {
+    const payload = separateToken(req.get('Authentication')).payload;
+    req.auth = getIdByPayload(payload);
+    next();
+};
--- src/apis/middlewares/index.js
+++ src/apis/middlewares/index.js
@@ -1,7 +1,9 @@
 const middlewares = require('express').Router();
 
 const token_verification_service = require('./tokenVerification');
+const authExtractor = require('./authExtractor');
 
 middlewares.use(token_verification_service);
+middlewares.use(authExtractor);
 
 module.exports = middlewares;
