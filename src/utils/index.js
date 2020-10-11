11b6f3c6e3981a156b3f2cbb4c77b12b780f2bdf
Jungwoo-Son <jungwoo9356@gmail.com>
2020-10-12 08:40:00 +0900

    [ADD] newToday

--- src/utils/index.js
+++ src/utils/index.js
@@ -23,21 +23,25 @@ function dateToString(date) {
 function isBetweenDate(a_date, b_date, target_date) {
     let a_time = a_date.getTime();
     let b_time = b_date.getTime();
-    const target_time = target_date;
+    const target_time = target_date.getTime();
 
     if(a_time > b_time) {
         const temp_time = b_time;
         b_time = a_time;
         a_time = temp_time;
     }
+    b_time += 86400000 - 1;
 
-    return a_time < target_time && b_time > target_time;
+    return a_time <= target_time && b_time >= target_time;
 }
 
-
+function newToday() {
+    return new Date(dateToString(new Date()));
+}
 
 module.exports = {
     validDateString,
     dateToString,
-    isBetweenDate
+    isBetweenDate,
+    newToday
 }
