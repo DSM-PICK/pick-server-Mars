3a81cd858736ceb9e0fcf8ea7401960001d02eee
Jungwoo-Son <jungwoo9356@gmail.com>
2020-10-11 22:48:24 +0900

    [ADD] isBetweenDate

--- src/utils/index.js
+++ src/utils/index.js
@@ -20,9 +20,24 @@ function dateToString(date) {
     return [year, month, day].join('-');
 }
 
+function isBetweenDate(a_date, b_date, target_date) {
+    let a_time = a_date.getTime();
+    let b_time = b_date.getTime();
+    const target_time = target_date;
+
+    if(a_time > b_time) {
+        const temp_time = b_time;
+        b_time = a_time;
+        a_time = temp_time;
+    }
+
+    return a_time < target_time && b_time > target_time;
+}
+
 
 
 module.exports = {
     validDateString,
-    dateToString
+    dateToString,
+    isBetweenDate
 }
