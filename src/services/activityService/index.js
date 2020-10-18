958f077d94bfce8ebf65d0c9f4820d57dcca9581
Jungwoo-Son <jungwoo9356@gmail.com>
2020-10-18 22:51:19 +0900

    [FIX] safe at holiday and weekend

--- src/services/activityService/index.js
+++ src/services/activityService/index.js
@@ -10,27 +10,27 @@ class ActivityService {
 
     async getThisDateActivity(date) {
         let activity;
-        try{
+        try {
             activity = await this.activity_repository.findByDate(date);
         }
-        catch(e) {
-            if(e instanceof RepoError.NotFoundDataException){
+        catch (e) {
+            if (e instanceof RepoError.NotFoundDataException) {
                 throw new Exceptions.NotFoundDataException;
             }
             throw e;
         }
 
-        const null_name_teacher = {name: null};
+        const null_name_teacher = { name: null };
         let floor2_teacher;
         try {
             floor2_teacher = await this.teacher_repository.findById(activity.second_floor_teacher_id);
         } catch (e) {
             floor2_teacher = null_name_teacher;
         }
-        
+
         let floor3_teacher;
         try {
-            floor3_teacher  = await this.teacher_repository.findById(activity.third_floor_teacher_id);
+            floor3_teacher = await this.teacher_repository.findById(activity.third_floor_teacher_id);
         } catch (e) {
             floor3_teacher = null_name_teacher;
         }
@@ -49,39 +49,55 @@ class ActivityService {
             floor2: floor2_teacher.name,
             floor4: floor4_teacher.name
         }
-        
+
         return result;
     }
 
     async getThisMonthActivity(month) {
-        if(month < 0) {
+        if (month < 0) {
             throw new Exceptions.InvalidDateException;
         }
-        if(month > 13) {
+        if (month > 13) {
             throw new Exceptions.InvalidDateException;
         }
 
         const this_year = new Date().getFullYear();
         const year_to_search = month == 0 ? this_year - 1
-                             : month == 13 ? this_year + 1
-                             : this_year;
+            : month == 13 ? this_year + 1
+                : this_year;
         const month_to_search = month == 0 ? 12
-                              : month == 13 ? 1
-                              : month;
+            : month == 13 ? 1
+                : month;
 
         let activities;
         try {
-            activities = await this.activity_repository.findByYearAndMonth(year_to_search, month_to_search);   
+            activities = await this.activity_repository.findByYearAndMonth(year_to_search, month_to_search);
         } catch (e) {
             throw new Exceptions.NotFoundDataException;
             //throw e;
         }
-
+        const null_name_teacher = { name: null };
         activities = activities.map(async (activity) => {
-            const floor2_teacher = await this.teacher_repository.findById(activity.second_floor_teacher_id);
-            const floor3_teacher = await this.teacher_repository.findById(activity.third_floor_teacher_id);
-            const floor4_teacher = await this.teacher_repository.findById(activity.forth_floor_teacher_id);
+            let floor2_teacher;
+            try {
+                floor2_teacher = await this.teacher_repository.findById(activity.second_floor_teacher_id);
+            } catch (e) {
+                floor2_teacher = null_name_teacher;
+            }
 
+            let floor3_teacher;
+            try {
+                floor3_teacher = await this.teacher_repository.findById(activity.third_floor_teacher_id);
+            } catch (e) {
+                floor3_teacher = null_name_teacher;
+            }
+
+            let floor4_teacher;
+            try {
+                floor4_teacher = await this.teacher_repository.findById(activity.forth_floor_teacher_id);
+            } catch (e) {
+                floor4_teacher = null_name_teacher;
+            }
 
 
             return {
@@ -95,9 +111,9 @@ class ActivityService {
         activities = await Promise.all(activities);
 
         const url = 'https://dsm-pick/activity/months/';
-        const prev_month_url = month > 0? url + (month - 1) : null;
-        const next_month_url = month < 13? url + (month + 1) : null;
-        
+        const prev_month_url = month > 0 ? url + (month - 1) : null;
+        const next_month_url = month < 13 ? url + (month + 1) : null;
+
         const result = {
             month: month,
             prev_month: prev_month_url,
