75080431ec20c211f46ccd7d935efba1903a2e76
Jungwoo-Son <jungwoo9356@gmail.com>
2020-11-12 16:16:44 +0900

    [ADD] add test case, reversed dates in isBetweenDate

--- src/tests/utils.test.js
+++ src/tests/utils.test.js
@@ -8,6 +8,11 @@ describe('Utils test', () => {
                 utils.isBetweenDate(new Date('2020-11-12'), new Date('2020-11-15'), new Date('2020-11-13')),
                 true);
         });
+        it('is between with reversed date', () => {
+            assert.strictEqual(
+                utils.isBetweenDate(new Date('2020-11-15'), new Date('2020-11-12'), new Date('2020-11-13')),
+                true);
+        });
         it('is not between', () => {
             assert.strictEqual(
                 utils.isBetweenDate(new Date('2020-11-12'), new Date('2020-11-15'), new Date('2020-11-17')),
