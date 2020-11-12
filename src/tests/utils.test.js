f000593b79bd63f830888683863f424a55bd6799
Jungwoo-Son <jungwoo9356@gmail.com>
2020-11-12 15:25:40 +0900

    [ADD] new test isBetweenDate in utils

---
+++ src/tests/utils.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const utils = require('../utils');
+
+describe('Utils test', () => {
+    describe('isBetweenDate test', () => {
+        it('is between', () => {
+            assert.strictEqual(
+                utils.isBetweenDate(new Date('2020-11-12'), new Date('2020-11-15'), new Date('2020-11-13')),
+                true);
+        });
+        it('is not between', () => {
+            assert.strictEqual(
+                utils.isBetweenDate(new Date('2020-11-12'), new Date('2020-11-15'), new Date('2020-11-17')),
+                false);
+        });
+    });
+});
