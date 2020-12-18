483053c5db17117929629b3b15e8cfc076c806e3
Jungwoo-Son <jungwoo9356@gmail.com>
2020-12-18 18:37:47 +0900

    [ADD] DateRange

---
+++ src/utils/range.js
@@ -0,0 +1,22 @@
+const { InvalidRange } = require('../errors/utils');
+
+class Range {
+    static isConflict(range1, range2) {
+        return (range1.start <= range2.end && range1.start >= range2.start)
+        || (range1.end <= range2.end && range1.end >= range2.start)
+        || (range1.start >= range2.start && range1.end <= range2.start)
+        || (range1.start >= range2.end && range1.end <= range2.end);
+    }
+}
+
+class DateRange extends Range{
+    constructor(date1, date2) {
+        if (date1 > date2) {
+            throw InvalidRange(`${date1} is not faster than ${date2}`);
+        }
+    }
+}
+
+module.exports = {
+    DateRange
+}
