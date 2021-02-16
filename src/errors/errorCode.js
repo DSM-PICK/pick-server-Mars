7123b32cb2bfc41773ef6a7953df8cd175c76cd5
Jungwoo-Son <jungwoo9356@gmail.com>
2021-02-17 01:45:34 +0900

    [ADD] error codes for new architecture

---
+++ src/errors/errorCode.js
@@ -0,0 +1,10 @@
+module.exports = {
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+    
+    NON_TOKEN: 401,
+    EXPIRED_TOKEN: 410,
+    INVALID_TOKEN: 403
+
+};
---
+++ src/errors/requestExceptions.js
@@ -0,0 +1,33 @@
+const ErrorCodes = require('./errorCode');
+
+class RequestException extends Error {
+    constructor(message, error_code) {
+        super(message);
+        this.error_code = error_code;
+    }
+}
+class BadRequestException extends RequestException {
+    constructor(message) {
+        if (message) {
+            super(message, ErrorCodes.BAD_REQUEST);
+        }
+        else {
+            super("전달된 값이 올바르지 않습니다.", ErrorCodes.BAD_REQUEST);
+        }
+    }
+}
+class InvalidFloorException extends BadRequestException {
+    constructor() {
+        super('올바르지 않은 층입니다.');        
+    }
+}
+class InvalidDateException extends BadRequestException {
+    constructor() {
+        super('올바르지 않은 날짜 형식입니다.');        
+    }
+}
+class InvalidTermException extends BadRequestException {
+    constructor() {
+        super('올바르지 않은 기간입니다.');        
+    }
+}
