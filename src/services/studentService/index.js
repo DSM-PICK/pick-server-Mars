76e44242406d43ae2839d498a65080f3e076bab8
Jungwoo-Son <jungwoo9356@gmail.com>
2021-02-17 19:50:47 +0900

    [UPDATE] change position of code

--- src/services/studentService/index.js
+++ src/services/studentService/index.js
@@ -5,28 +5,9 @@ class StudentService {
         this.student_repository = student_repository;
     }
 
-    async expect_by_input(input_data) {
-        const datas = input_data.split(' ').filter((data) => {
-            return data; // remove disturbed data like ''
-        });
-        if(datas.length > 2 || datas.length < 1) {
-            throw new Exceptions.InvalidGivenDataException;
-        }
-
-        let number = undefined;
-        let name = undefined;
-        for(const data of datas) {
-            if(isNaN(Number(data)) == false) {
-                number = data;
-            }
-            else {
-                name = data;
-            }
-        }
-
-
+    async expect_by_input(number, name) {
         let finded_students;
-        if(number != undefined && datas.length == 2) {
+        if(number != undefined && name != undefined) {
             finded_students = await this.student_repository.findAllByNumberAndName(number, name);
         }
         else if(number != undefined) {
