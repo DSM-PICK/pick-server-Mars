3b302b855ff5d6bc75a91e7df4f5a5975f68b716
Jungwoo-Son <jungwoo9356@gmail.com>
2021-04-01 22:48:06 +0900

    [UPDATE] 기초학력일때 출석 반영 막음

--- src/repositories/definitions/attendance.js
+++ src/repositories/definitions/attendance.js
@@ -1,16 +1,16 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
+const { Sequelize, Model, DataTypes, Op } = require('sequelize');
 const { sequelize } = require('../../loaders/database');
 
 class Attendance extends Model {
     static async updateAttendance(date, student, period, state, reason) {
         return await Attendance.update({state: state, reason: reason, memo: null}, {
-            where: { date: date, student_num: student, period: period }
+            where: { date: date, student_num: student, period: period, state: { [Op.not]: '기초학력'} }
         });
     }
 
     static async updateAttendanceToMoving(date, student, period, memo) {
         return await Attendance.update({state: '이동', memo: memo, reason: null}, {
-            where: { date: date, student_num: student, period: period }
+            where: { date: date, student_num: student, period: period, state: { [Op.not]: '기초학력'} }
         });
     }
 }
